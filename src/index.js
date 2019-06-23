bebbfca27d0d90c8df320cca60ef3bd2f03e553d
Tanvi Garg <tanvi.garg23@gmail.com>
2019-06-23 00:45:13 -0700

    create saga middlerware and apply to redux store

--- src/index.js
+++ src/index.js
@@ -4,9 +4,34 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, applyMiddleware, compose } from "redux";
+import createSagaMiddleware from "redux-saga";
+import { Provider } from "react-redux";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+import { reducer } from "./redux";
+import { watcherSaga } from "./sagas";
+
+// createSagaMiddleware, and apply it to the Redux store with some help from compose and applyMiddleware
+const sagaMiddleware = createSagaMiddleware();
+
+// dev tools middleware
+const reduxDevTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+// create a redux store with imported reducer above and middleware
+let store = createStore(
+    reducer,
+    compose(applyMiddleware(sagaMiddleware), reduxDevTools)
+);
+
+// run the saga so that it can trigger the workerSaga when there’s an API_CALL_REQUEST
+sagaMiddleware.run(watcherSaga);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+
 serviceWorker.unregister();
